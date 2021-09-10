725067990f57a075104c560c9366692f9b31fc8c
brunstart <brunstart@gmail.com>
2021-09-11 01:49:44 +0900

    210911

---
+++ BFS(Breadth first search).py
@@ -0,0 +1,26 @@
+graph_list = {1: set([2,3,4]),
+              2: set([1,5]),
+              3: set([1,6,7]),
+              4: set([1,8]),
+              5: set([2, 9]),
+              6: set([3, 10]),
+              7: set([3]),
+              8: set([4]),
+              9: set([5]),
+              10: set([6])}
+root_node = 1
+
+from collections import deque
+
+def BFS(graph, root):
+    visit = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visit:
+            visit.append(n)
+            queue += graph[n] - set(visit)
+    return visit
+
+print(BFS(graph_list, root_node))
